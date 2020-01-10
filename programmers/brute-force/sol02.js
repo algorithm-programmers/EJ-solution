89e59deca4a529b8550dd5d42bc70c44e58a45c8
ieunjung <euju718@gmail.com>
2020-01-11 00:20:49 +0900

    소수찾기 진행중..

---
+++ programmers/brute-force/sol02.js
@@ -0,0 +1,11 @@
+function isPrime(n){
+    let count = 0;
+    for(let i = 1; i <= n; i++){
+        if(n%i === 0){
+            count ++;
+        }
+    }
+    return count === 2 ? true : false;
+}
+
+console.log(isPrime(17));
