a87e822ee880b6f64203449ebc439c17af93c1ef
ieunjung <euju718@gmail.com>
2020-02-13 18:37:27 +0900

    스택/큐 - 다리를 지나는 트럭 푸는중

--- programmers/stack/sol02.js
+++ programmers/stack/sol02.js
@@ -1,25 +1,25 @@
 function solution(bridge_length, weight, truck_weights) {
     let answer = 0;
     let finished_trucks = [], moving_trucks = [];
-    for (let i = 0; i < truck_weights.length; i++) {
-        const element = truck_weights[i];
-        if(moving_trucks.length === 0){
-            moving_trucks.push(element);
-        }
-        let n = bridge_length;
-        while(moving_trucks && n > 0){
-            const prev_element = truck_weights[i+1];
-            if(weight > moving_trucks[0] + prev_element){
-                moving_trucks.push(prev_element);
-                n += bridge_length;
+    moving_trucks.push({weight: truck_weights.shift(), steps: bridge_length});
+    while(moving_trucks.length > 0){
+        let weight_sum = 0;
+        for (const moving_truck in moving_trucks) {
+            if (moving_trucks.hasOwnProperty(moving_truck)) {
+                const element = moving_trucks[moving_truck];
+                weight_sum += element;
             }
-            answer++;
-            n--;
         }
+
+        if(weight_sum < weight){
+            moving_trucks.push({weight: truck_weights.shift(), steps: bridge_length});
+        }
+
+        answer++;
     }
     console.log(answer);
     return answer;
 }
 
-// solution(2, 10, [7,4,5,6]);
-solution(100, 100, [10]);
+solution(2, 10, [7,4,5,6]);
+// solution(100, 100, [10]);
