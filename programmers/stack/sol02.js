7dcc7dd3d8e749952015f3fd082e08077fbd7c14
ieunjung <euju718@gmail.com>
2020-02-14 16:33:17 +0900

    스택/큐 - 다리를 지나는 함수 - 케이스 3번이 틀리다

--- programmers/stack/sol02.js
+++ programmers/stack/sol02.js
@@ -1,20 +1,34 @@
+/**
+ * 스택/큐 - 다리를 지나는 함수
+ * https://programmers.co.kr/learn/courses/30/lessons/42583?language=javascript
+ * @param {number} bridge_length 다리 길이
+ * @param {number} weight 다리가 견딜 수 있는 무게
+ * @param {Array} truck_weights 트럭별 무게, 대기 트럭 배열
+ */
 function solution(bridge_length, weight, truck_weights) {
-    let answer = 0;
-    let finished_trucks = [], moving_trucks = [];
-    moving_trucks.push({weight: truck_weights.shift(), steps: bridge_length});
-    while(moving_trucks.length > 0){
-        let weight_sum = 0;
-        for (const moving_truck in moving_trucks) {
-            if (moving_trucks.hasOwnProperty(moving_truck)) {
-                const element = moving_trucks[moving_truck];
-                weight_sum += element;
-            }
-        }
+    let answer = 0; // 모든 대기 트럭이 다리를 건너는 시간
+    let moving_trucks = [];
+    
+    // 대기 트럭이 있거나 다리를 건너는 트럭이 있는 동안의 시간을 잰다.
+    while(truck_weights.length > 0 || moving_trucks.length > 0){
 
-        if(weight_sum < weight){
+        let moving_weight = 0;  // 다리를 건너고 있는 트럭 무게의 합
+
+        moving_trucks.map(moving_truck => {
+            moving_weight += moving_truck.weight;
+            moving_truck.steps--;
+            if(moving_truck.steps === 0){
+                moving_weight -= moving_trucks.shift().weight;
+            }
+        });
+        
+        const curr_truck = truck_weights[0];
+        if(moving_weight + curr_truck <= weight){
+            moving_weight += curr_truck;
             moving_trucks.push({weight: truck_weights.shift(), steps: bridge_length});
         }
-
+        
+        console.log(truck_weights, moving_trucks, moving_weight, weight);
         answer++;
     }
     console.log(answer);
@@ -23,3 +37,4 @@ function solution(bridge_length, weight, truck_weights) {
 
 solution(2, 10, [7,4,5,6]);
 // solution(100, 100, [10]);
+// solution(100, 100, [10,10,10,10,10,10,10,10,10,10]);
