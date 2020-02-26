7ad4cb5f04865efa7881afe981284416e516ac7e
ieunjung <euju718@gmail.com>
2020-02-26 21:00:12 +0900

    브루트 포스 - 테트로미노 푸는 중

---
+++ codeplus/basic02/step01/study05.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * study05 테트로미노
+ * https://www.acmicpc.net/problem/14500
+ */
+public class study05 {
+    // 19가지 모양의 도형
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                
+            }
+        }
+    }
+}
