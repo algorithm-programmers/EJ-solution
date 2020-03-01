2a64cc9a50b0619920c664627e5bb40008d23463
ieunjung <euju718@gmail.com>
2020-03-02 02:26:40 +0900

    N과 M (3)

---
+++ codeplus/basic02/step02/study03.java
@@ -0,0 +1,36 @@
+/**
+ * study03 N과 M (3)
+ * https://www.acmicpc.net/problem/15651
+ * 중복 선택 가능! (1)에서 중복 처리했던 부분 다 삭제.
+ */
+public class study03 {
+    // static boolean[] c = new boolean[10];
+    static int[] a = new int[10];
+    static StringBuilder go(int index, int n, int m) {
+        if (index == m) {
+            StringBuilder sb = new StringBuilder();
+            for (int i=0; i<m; i++) {
+                sb.append(a[i]);
+                if (i != m-1) sb.append(" ");
+            }
+            sb.append("\n");
+            return sb;
+        }
+        StringBuilder ans = new StringBuilder();
+        for (int i=1; i<=n; i++) {
+            // if (c[i]) continue;
+            // c[i] = true;
+            a[index] = i;
+            ans.append(go(index+1, n, m));
+            // c[i] = false;
+        }
+        return ans;
+    }   
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        System.out.print(go(0,n,m));
+    }
+}
