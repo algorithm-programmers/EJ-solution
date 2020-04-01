a7a9fc6439551a3315aa8736214306906331ddbc
ieunjung <euju718@gmail.com>
2020-04-01 23:49:11 +0900

    푸는중

---
+++ programmers/brute-force/sol03.js
@@ -0,0 +1,14 @@
+/**
+ * 숫자 야구
+ * 
+ * @param {Array.<Array<number>} baseball 
+ */
+function solution(baseball) {
+    var answer = 0;
+
+    baseball
+
+    return answer;
+}
+
+solution([[123, 1, 1], [356, 1, 0], [327, 2, 0], [489, 0, 1]]);
--- programmers/sort/sol03.js
+++ programmers/sort/sol03.js
@@ -1,6 +1,7 @@
 /**
  * H-Index : 과학자의 생산성과 영향력을 나타내는 지표.
  * 어떤 과학자가 발표한 논문 n편 중, "h번 이상 인용된 논문이 h편 이상"이고 나머지 논문이 h번 이하 인용되었다면 h의 최댓값이 이 과학자의 H-Index입니다.
+ * 참고) https://caution-dev.tistory.com/11
  * 
  * @param {Array} citations 논문의 인용 횟수
  */
