71a8a3a071da50a7e1d484856299ef03b1c7611d
ieunjung <euju718@gmail.com>
2020-04-05 03:01:55 +0900

    완전탐색-소수찾기 수정

--- programmers/brute-force/sol02.js
+++ programmers/brute-force/sol02.js
@@ -1,61 +1,38 @@
 /**
  * 소수 찾기
- * 
+ *
  * @param {string} numbers 임의의 숫자
  */
 function solution(numbers) {
-  var answer = 0;
+  let answer = 0;
 
-  let tmpArr = [];
   const numberArr = numbers.split("");
-  for (let i = 1; i < numberArr.length; i++) {
-    makeNumberArr(numberArr, 0, i, tmpArr);
+  let tmpArr = new Set();
+  combi(numberArr, "");
+
+  function combi(arr, s) {
+    if (s.length > 0) {
+      s = Number(s);
+      if (!tmpArr.has(s)) {
+        tmpArr.add(s);
+        if (isPrime(s)) {
+          answer++;
+        }
+      }
+    }
+
+    if (arr.length > 0) {
+      for (let i = 0; i < arr.length; i++) {
+        let t = arr.slice(0);
+        t.splice(i, 1);
+        combi(t, s + arr[i]);
+      }
+    }
   }
 
-  console.log(tmpArr);
-  answer = tmpArr.length;
+  console.log(answer);
 
   return answer;
-}
-
-// solution("17");
-solution("177");
-// solution("011");
-
-function makeNumberArr(numArr, depth, purposeNum, tmpArr) {
-  console.log(numArr, '/depth =>', depth, '/purpose =>', purposeNum, tmpArr);
-
-  if (depth === purposeNum) {
-    // 끝
-
-    let strBuiler = [];
-    for (let i = 0; i < purposeNum; i++) {
-        strBuiler.push(numArr[i]);
-    }
-    strBuiler = Number(strBuiler.join(""));
-    if(isPrime(strBuiler) && tmpArr.indexOf(strBuiler) === -1){
-        tmpArr.push(strBuiler)
-    }
-
-    const numberStr = Number(numArr.join(""));
-    if (isPrime(numberStr) && tmpArr.indexOf(numberStr) === -1) {
-      tmpArr.push(numberStr);
-    }
-
-    return;
-  }
-
-  for (let i = depth; i < numArr.length; i++) {
-    swap(numArr, i, depth);
-    makeNumberArr(numArr, depth + 1, purposeNum, tmpArr);
-    swap(numArr, i, depth); //  배열을 원래 상태로 돌린다.
-  }
-}
-
-function swap(arr, i, j) {
-  const tmp = arr[i];
-  arr[i] = arr[j];
-  arr[j] = tmp;
 }
 
 function isPrime(n) {
@@ -67,3 +44,9 @@ function isPrime(n) {
   }
   return count === 2 ? true : false;
 }
+
+// solution("17");
+// solution("177");
+// solution("71");
+solution("011");
+// solution("13");
