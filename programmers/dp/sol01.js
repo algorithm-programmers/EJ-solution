44553466ddf812e7cb9b3390348372124b9e83f5
ieunjung <euju718@gmail.com>
2020-04-19 03:59:05 +0900

    N으로 표현

--- programmers/dp/sol01.js
+++ programmers/dp/sol01.js
@@ -1,25 +1,29 @@
 /**
  * N으로 표현
- * 
+ *
  * @param {number} N 사칙연산에 이용할 숫자
  * @param {number} number 연산하여 구할 숫자
  */
 function solution(N, number) {
-    let answer = 0;
-    let dp = [];
-    //
-    let ops = [];
-    ops.push(N + N);
-    ops.push(N - N);
-    ops.push(N * N);
-    ops.push(N / N);
-
-    console.log(ops);
-    
-    
-
-    return answer;
+  const cache = new Array(9).fill(0).map((el) => new Set());
+  for (let i = 1; i < 9; i++) {
+    cache[i].add("1".repeat(i) * N);
+    for (let j = 0; j < i; j++) {
+        for(const arg1 of cache[j]){
+            for(const arg2 of cache[i-j]){
+                cache[i].add(arg1 + arg2);
+                cache[i].add(arg1 - arg2);
+                cache[i].add(arg1 * arg2);
+                cache[i].add(arg1 / arg2 >> 0);
+            }
+        }
+    }
+    if(cache[i].has(number)){
+        return i;
+    }
+  }
+  return -1;
 }
 
-solution(5, 12);    // 4
+console.log(solution(5, 12));   //  4
 // solution(2, 11);
