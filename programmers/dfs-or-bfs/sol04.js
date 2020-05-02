c058892e109a7c7905ce8d8546eaa2ce780af00c
ieunjung <euju718@gmail.com>
2020-05-03 03:06:38 +0900

    테스트 통과 정확성 테스트 실패

--- programmers/dfs-or-bfs/sol04.js
+++ programmers/dfs-or-bfs/sol04.js
@@ -1,33 +1,43 @@
 /**
  * 여행 경로
+ * "주어진 항공권을 모두 이용하여 여행경로를 짜려고 합니다."
+ * "만일 가능한 경로가 2개 이상일 경우 알파벳 순서가 앞서는 경로를 return 합니다."
  * 
  * @param {String[][]} tickets 항공권 정보
  */
 function solution(tickets) {
     let answer = [];
     
-    // 첫번째 공항은 ICN 으로 고정
-    answer.push(tickets[0][0]);
-    answer.push(tickets[0][1]); 
-
+    // 도착지 알파벳 순 정렬
+    tickets.sort((a, b) => a[1].localeCompare(b[1]));   
+    // console.log(tickets);
+    
     const length = tickets.length;
 
     let checked = new Array(length).fill(false);
+
+    // 첫번째 공항은 ICN 으로 고정
     const icnIdx = tickets.findIndex(tickek => tickek[0] === "ICN");
-    checked[icnIdx] = true;  // 첫번째 공항 ICN 고정
-    
-    function getNextStep(prevStep){
-        tickets.map((ticket, idx) => {
-            if(!checked[idx] && ticket[0] === prevStep){
+    checked[icnIdx] = true;
+
+    const icnEl = tickets.find(ticket => ticket[0] === "ICN");
+    answer.push(icnEl[0]);
+    answer.push(icnEl[1]);
+
+    function getNextStep(prevStep) {
+        tickets.some((ticket, idx) => {
+            if (!checked[idx] && ticket[0] === prevStep) {
+                // console.log(prevStep, ticket, idx);
                 answer.push(ticket[1]);
                 checked[idx] = true;
+                return true;
             }
         });
     }
 
     for (let i = 0; i < length; i++) {
         getNextStep(answer.slice(-1)[0]);   // array.slice(-1)[0] : last element of Array.
-        // console.log(i, answer);
+        
     }
 
     console.log(answer);
@@ -35,5 +45,5 @@ function solution(tickets) {
     return answer;
 }
 
-solution([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]);
-// solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]])
+// solution([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]);
+solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL", "SFO"]]);
