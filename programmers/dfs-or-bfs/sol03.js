a55041970a7a09ac1e9045e32001b23930badffb
ieunjung <euju718@gmail.com>
2020-05-02 23:58:36 +0900

    푸는중

---
+++ programmers/dfs-or-bfs/sol03.js
@@ -0,0 +1,16 @@
+/**
+ * 단어 변환
+ * 
+ * @param {String} begin 시작 단어
+ * @param {String} target 변환 목표 단어
+ * @param {String[]} words 단어의 집합
+ */
+function solution(begin, target, words) {
+    let answer = 0;
+
+    let check = new Array(words.length).fill(false);
+
+    return answer;
+}
+
+solution("hit", "cog", [hot, dot, dog, lot, log, cog]);     // 4
