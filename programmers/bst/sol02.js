961fffaa1911dd43d825182cfbe3adf13d1e5311
ieunjung <euju718@gmail.com>
2020-05-14 01:13:34 +0900

    테케 일부 실패

--- programmers/bst/sol02.js
+++ programmers/bst/sol02.js
@@ -5,31 +5,35 @@
  * @param {number[]} times 심사관 배열
  */
 function solution(n, times) {
-    let answer = 0;
-
     let left = 1;
-    let right = times[times.length - 1] * n;
+    let right = Math.max(...times) * n; // 느린 심사관이 모든 사람들을 심사하는 경우
+
+    let answer = right;
 
     while(left <= right){
         const mid = (left + right) / 2;
-        let done = times.reduce((prev, curr) => prev + parseInt(mid / curr));
-
-        console.log(done);
+        let done = times.reduce((prev, curr) => prev + parseInt(mid / curr), 0);
         
-        if(done < n){
-            left = mid + 1;
-        }else{
+        console.log(mid, left, right, done);
+        
+        // 심사관이 더 맡을 수 있다면 시간을 줄인다.
+        if(done >= n){
             if(mid < answer){
                 answer = mid;
             }
-
             right = mid - 1;
+        }
+        // 시간 늘림
+        else{
+            left = mid + 1;
         }
     }
 
+    answer = Math.floor(answer)
     console.log(answer);
     
     return answer;
 }
 
-solution(6, [7, 10]);   // 28
+// solution(6, [7, 10]);   // 28
+solution(1, [2, 2]);
