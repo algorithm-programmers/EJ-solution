99de67042f5fcfd8920ec86dde6d63f289efde50
ieunjung <euju718@gmail.com>
2020-05-18 23:55:41 +0900

    다시!

---
+++ programmers/others/124나라의숫자.js
@@ -0,0 +1,13 @@
+/**
+ * 124 나라의 숫자
+ * 
+ * @param {number} n 자연수
+ */
+function solution(n) {
+    
+}
+
+solution(1);    //1
+// solution(2);    //2
+// solution(3);    //4
+// solution(4);    //11
