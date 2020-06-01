850c3802064989732e1d0395da795a84f755e7a5
ieunjung <euju718@gmail.com>
2020-06-02 00:02:23 +0900

    같은 숫자는 싫어

--- programmers/others/같은숫자는싫어.js
+++ programmers/others/같은숫자는싫어.js
@@ -8,23 +8,24 @@ function solution(arr) {
 
     let result = [];
     result.push(arr[0]);
+
     for (let i = 0; i < length; i++) {
         const el1 = arr[i];
-        console.log(el1);
-        
+        const el2 = arr[i+1];
 
-        for (let j = i; j < length; j++) {
-            const el2 = arr[j];
-            if(el1 === el2){
-                return;
-            }else{
-                result.push(el2);
-            }
+        if(el1 === el2){
+            continue;
+        }else{
+            el2 !== undefined && result.push(el2);
         }
     }
 
     console.log(result);
-    
+    return result;
 }
 
 solution([1,1,3,3,0,1,1]); // [1,3,0,1]
+
+
+// 한 줄 필터링
+const filtered = (arr) => arr.filter((el, idx) => el !== arr[idx + 1]);
