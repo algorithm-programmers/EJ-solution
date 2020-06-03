91c7072dffd08af3cf699301a02db9286f407612
ieunjung <euju718@gmail.com>
2020-06-03 23:24:55 +0900

    다트게임... -경우의 수를 더 추가해봤지만 에러

--- programmers/others/다트게임.js
+++ programmers/others/다트게임.js
@@ -16,11 +16,13 @@ function solution(dartResult) {
         let bonusEl = arr[i+1];
         let optEl = arr[i+2];
 
-        if(numEl){
+        if(numEl >= 0){
             if(numEl === 1 && bonusEl === '0'){
                 numEl = 10;
                 bonusEl = arr[i+2];
                 optEl = arr[i+3];
+            }else if(numEl === 0 && arr[i-1] === '1'){
+                continue;
             }
             console.log(numEl, bonusEl, optEl);
 
@@ -36,22 +38,19 @@ function solution(dartResult) {
             let currNum = Math.pow(numEl, bonus);
 
             if(optEl === '*'){
-                arrNum[arrNum.length - 1] *= 2;
                 currNum *= 2;
-                console.log('===');
-                console.log(arrNum.length - 1, arrNum[arrNum.length - 1]);
+                if(arrNum.length - 1 > -1){
+                    arrNum[arrNum.length - 1] *= 2;
+                }
                 
             }else if(optEl === '#'){
                 currNum *= -1;
             }
-            console.log(currNum);
-            
             arrNum.push(currNum);
         }
         console.log(arrNum);
     }
     console.log('answer :', arrNum.reduce((prev, curr) => prev + curr));
-    
 
     return arrNum.reduce((prev, curr) => prev + curr);
 }
@@ -59,7 +58,12 @@ function solution(dartResult) {
 // solution("1S2D*3T");  // 37
 // solution("1D2S#10S");  // 9
 // solution("1D2S0T");  // 3
-solution("1S*2T*3S");   // 23
+// solution("1S*2T*3S");   // 23
+// solution("1D#2S*3S");   //5
+// solution("1T2D3D#");    // -4
+// solution("1D2S3T*");    // 59
+// solution("1T#2D*0S");   // (-1^3 + 2^2 ) * 2 + 0^1 = (-1 + 4)*2 = 6
+solution("10S10D*10T#");  // 10^1 + -10^2 * 2 + 10^3 * 2 = 10 + -100 + 1000 = 100
 /* 
 
 1^1 * 2(* 효과) + 2^2 * 2(* 효과) + 3^3
