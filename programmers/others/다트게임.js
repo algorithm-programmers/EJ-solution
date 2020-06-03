9972792447d13d8b709ac0c1bc8c60aa4ef56a1d
ieunjung <euju718@gmail.com>
2020-06-04 00:10:03 +0900

    다트게임 -정규식 이용! 토큰화 시키기

--- programmers/others/다트게임.js
+++ programmers/others/다트게임.js
@@ -1,74 +1,69 @@
 /**
  * 다트 게임
+ * 다트를 세 번 던져서 계산한다.
+ * 
  * Single(S) : 1제곱
  * Double(D) : 2제곱 
  * Triple(T) : 3제곱 
  * 스타상(*) : 해당 점수와 바로 전 점수를 2배로 만든다
  * 아차상(#) : 해당 점수는 마이너스가 된다.
  * 
+ * 참고 : https://jo-c.tistory.com/23
+ * 
  * @param {*} dartResult 
  */
 function solution(dartResult) {
-    const arr = dartResult.split("");
-    let arrNum = [];
-    for (let i = 0; i < arr.length; i++) {
-        let numEl = parseInt(arr[i]);
-        let bonusEl = arr[i+1];
-        let optEl = arr[i+2];
+    const regexp = /\d{1,2}[SDT]{1}[*|#]?/g;
+    const dartArr = dartResult.match(regexp);
+    console.log(dartArr);
+    
+    let score = [];
+    for (let i = 0; i < 3; i++) {
+        const dart = dartArr[i];
+        let num = dart.match(/\d{1,2}/g);
+        const bonus = dart.match(/[SDT]{1}/g);
+        const opt = dart.match(/[*|#]/g);
 
-        if(numEl >= 0){
-            if(numEl === 1 && bonusEl === '0'){
-                numEl = 10;
-                bonusEl = arr[i+2];
-                optEl = arr[i+3];
-            }else if(numEl === 0 && arr[i-1] === '1'){
-                continue;
-            }
-            console.log(numEl, bonusEl, optEl);
+        console.log(num, bonus, opt);
 
-            let bonus = 1;
-            if(bonusEl === 'S'){
-                bonus = 1;
-            }else if(bonusEl === 'D'){
-                bonus = 2;
-            }else if(bonusEl === 'T'){
-                bonus = 3;
-            }
-
-            let currNum = Math.pow(numEl, bonus);
-
-            if(optEl === '*'){
-                currNum *= 2;
-                if(arrNum.length - 1 > -1){
-                    arrNum[arrNum.length - 1] *= 2;
-                }
-                
-            }else if(optEl === '#'){
-                currNum *= -1;
-            }
-            arrNum.push(currNum);
+        if(bonus == 'S'){
+            num = parseInt(num);;
+        }else if(bonus == 'D'){
+            num = num * num;
+        }else if(bonus == 'T'){
+            num = num * num * num;
         }
-        console.log(arrNum);
-    }
-    console.log('answer :', arrNum.reduce((prev, curr) => prev + curr));
 
-    return arrNum.reduce((prev, curr) => prev + curr);
+        if(opt == '*'){
+            num *= 2;
+            if(i > -1){
+                score[i-1] *= 2;
+            }
+        }else if(opt == '#'){
+            num *= -1;
+        }
+        score.push(num);
+    }
+    console.log(score, score.reduce((prev, curr) => prev + curr));
+
+    return score.reduce((prev, curr) => prev + curr);
 }
 
-// solution("1S2D*3T");  // 37
+solution("1S2D*3T");  // 37
 // solution("1D2S#10S");  // 9
 // solution("1D2S0T");  // 3
 // solution("1S*2T*3S");   // 23
 // solution("1D#2S*3S");   //5
 // solution("1T2D3D#");    // -4
 // solution("1D2S3T*");    // 59
-// solution("1T#2D*0S");   // (-1^3 + 2^2 ) * 2 + 0^1 = (-1 + 4)*2 = 6
-solution("10S10D*10T#");  // 10^1 + -10^2 * 2 + 10^3 * 2 = 10 + -100 + 1000 = 100
 /* 
 
 1^1 * 2(* 효과) + 2^2 * 2(* 효과) + 3^3
 1^2 + 2^1 * (-1) + 10^1
 1^2 + 2^1 + 0^3
 (1^1 * 2 + 2^3) * 2 + 3^1
+
+숫자 범위에 두 자리수인 10도 포함되기 때문에
+카카오 해설대로 정규식을 이용해서 토큰화를 시키고 난 후 계산하는 것이 더 쉬웠다.
 
 */
