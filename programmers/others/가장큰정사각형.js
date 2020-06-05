448e2c349ad3ab5798d9f28a42f45a1c3b46e448
ieunjung <euju718@gmail.com>
2020-06-06 00:56:52 +0900

    가장 큰 정사각형 찾기

--- programmers/others/가장큰정사각형.js
+++ programmers/others/가장큰정사각형.js
@@ -1,29 +1,40 @@
 /**
  * 가장 큰 정사각형 찾기
+ * DP
+ * board[i][j]의 사방을 비교하면서 가능한 정사각형을 만든다.
+ * 참고 : https://codedrive.tistory.com/53
  * 
  * @param {number[][]} board 
  */
 function solution(board) {
+    let answer = 0;
 
-    for (let i = 0; i < board.length; i++) {
-        const row = board[i];
-        console.log(row);
+    const xLength = board[0].length;
+    const yLength = board.length;
+    if(xLength < 2 || yLength < 2){
+        return 1;   // 0 또는 1 이 나온다.
+    }
 
-        let x = 0, y = 0;
-        for (let j = x; j < row.length; j++) {
-            const element = row[j];
-            if(element !== 0){
-                console.log(i, j);
-                x = j;
-                y = i;
-                break;
+    for (let i = 1; i < xLength; i++) {
+        for (let j = 1; j < yLength; j++) {
+            if(board[i][j] > 0){
+                const min = Math.min(board[i-1][j], board[i-1][j-1], board[i][j-1]);
+                board[i][j] = min + 1;
             }
             
+            if(answer < board[i][j]){
+                answer = board[i][j];
+            }
         }
-        console.log('x : '+x + '/y :'+y);
-
     }
+    console.log(board, answer * answer);
+    
+    return answer * answer;
 }
 
-solution([[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]);    // 9
-solution([[0,0,1,1],[1,1,1,1]]);    // 4
+// solution([[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]);    // 9
+// solution([[0,0,1,1],[1,1,1,1]]);    // 4
+// solution([[1,1,0],[1,1,0],[0,0,0]]); // 4
+// solution([[1,0],[1,0]]);    // 0 => 점이므로 정사각형이 될 수 없다.
+// solution([[1],[1]]); // 1
+// solution([[0],[0]]); // 0
