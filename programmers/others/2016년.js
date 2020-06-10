fb3db01ad5394b458b79505c2ebba2c4415bd43f
ieunjung <euju718@gmail.com>
2020-06-10 23:55:31 +0900

    푸는중

---
+++ programmers/others/2016년.js
@@ -0,0 +1,13 @@
+/**
+ * 2016년 
+ * 윤년, 2016년 a월 b일은 무슨 요일인가?
+ * 예) 16/05/24 => 화요일
+ * 
+ * @param {number} a 월
+ * @param {number} b 일
+ */
+function solution(a, b) {
+    
+}
+
+solution(5, 24);    // "TUE"
