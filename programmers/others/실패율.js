3cd4cf0c3e40ea4b7677c919d5adb0a489273a4b
ieunjung <euju718@gmail.com>
2020-06-16 00:24:23 +0900

    2번 테스트 실패

--- programmers/others/실패율.js
+++ programmers/others/실패율.js
@@ -7,26 +7,41 @@
  */
 function solution(N, stages) {
     const totalUsers = stages.length;
+    // {스테이지 번호 : 멈춰있는 사용자 수, ...}
     const userLocation = stages.reduce((prev, curr) => {
-        if(curr > N){
+        if (curr > N) {
             return prev;
         }
         const count = prev[curr] ? prev[curr] + 1 : 1;
-        return {...prev, [curr]: count}
+        return { ...prev, [curr]: count }
     }, {});
 
-    console.log(userLocation);
-
-    let answer = [];
-    for (let i = 1; i <= totalUsers; i++) {
-        const count = userLocation[i];
-        if(count){
-            answer.push(i);
+    // [{스테이지 번호: 실패율 },...]
+    let failureArr = [];
+    for (let i = 1; i <= N; i++) {
+        let stagePlayedCount = 0;
+        for (let j = 1; j < i; j++) {
+            stagePlayedCount += userLocation[j];
         }
+
+        let rate = 0;
+        const count = userLocation[i];
+        if (count) {
+            // console.log(totalUsers, count);
+            rate = count / (totalUsers - stagePlayedCount);
+        } else {
+            rate = 0;
+        }
+        failureArr.push({ index: i, failureRate: rate });
     }
 
-    console.log(answer);
-    
+    const answer = failureArr.sort((a, b) => b.failureRate - a.failureRate).reduce((prev, curr) => {
+        return [...prev, curr.index];
+    }, []);
+
+    // console.log(answer);
+    return answer;
 }
 
-solution(5, [2, 1, 2, 6, 2, 4, 3, 3]);
+// solution(5, [2, 1, 2, 6, 2, 4, 3, 3]);
+solution(4, [4,4,4,4,4]);
