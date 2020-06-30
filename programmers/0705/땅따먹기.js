a7a67d2a6c29c203d7a61a1cd02d94b5399c165d
ieunjung <euju718@gmail.com>
2020-07-01 01:04:57 +0900

    실패 코드

--- programmers/0705/땅따먹기.js
+++ programmers/0705/땅따먹기.js
@@ -7,14 +7,29 @@
 function solution(land) {
     const NOT_SELECTED = -1;
     let prevIdx = NOT_SELECTED;
+
+    let sum = 0;
+    
     for (let i = 0; i < land.length; i++) {
         const row = land[i];
-        const max = Math.max(...row);
-        const maxIdx = row.indexOf(max);
 
-        console.log(row, max, maxIdx);
+        let max = 0, maxIdx = 0;
+        row.map((r, rIdx) => {
+            if(r > max && rIdx !== prevIdx){
+                max = r;
+                maxIdx = rIdx;
+            }
+        })
+
+        // console.log(row, max, maxIdx);
+        sum += max;
         prevIdx = maxIdx;
     }
+
+    console.log(sum);
+
+    return sum;
 }
 
-solution([[1,2,3,5],[5,6,7,8],[4,3,2,1]]);
+// solution([[1,2,3,5],[5,6,7,8],[4,3,2,1]]);
+solution([[1,2,3,5],[5,6,7,100],[4,3,2,1]]);
