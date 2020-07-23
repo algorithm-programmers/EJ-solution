dd1ec83475f6f8c5e058aa0d665917aaf829a3a2
ieunjung <euju718@gmail.com>
2020-07-24 01:29:50 +0900

    오픈채팅방

--- programmers/0726/오픈채팅방.js
+++ programmers/0726/오픈채팅방.js
@@ -1,33 +1,52 @@
 /**
  * 오픈채팅방
  * 최종적으로 방을 개설한 사람이 보게 되는 메시지를 문자열 배열 형태로 return 하도록 solution 함수를 완성하라.
- * 유저 아이디와 닉네임은 알파벳 대문자, 소문자를 구별한다.
  * 
  * @param {string} record 채팅방 유저 변경 정보가 담긴 문자열
  */
 function solution(record) {
-    let answer = [];
-    // {uid: '', name: '', action: '', showing_msg: '' }
+    let userInfo = [];
+    // {uid: username};
 
+    // 최신 유저 정보 업데이트
+    const ENTER = 'Enter', LEAVE = 'Leave', CHANGE = 'Chnage';
     for (let i = 0; i < record.length; i++) {
-        const [action, uid, username] = record[i].split(" ");
+        const [, uid, username] = record[i].split(" ");
 
-        let showingMsg = '';
-        if (action === 'Enter') {
-            showingMsg = username + "님이 들어왔습니다.";
-        } else if (action === 'Leave') {
-            showingMsg = username + "님이 나갔습니다.";
-        }
-
-        answer.push({ uid: uid, username: username, action: action, showing_msg: username + "님이 들어왔습니다." });
-
-        if (action === 'Change') {
-
+        if (username) {
+            userInfo[uid] = username;
         }
     }
+
+    console.log(userInfo);
+
+    // 출력
+    const ENTER_MSG = '님이 들어왔습니다.', LEAVE_MSG = '님이 나갔습니다.'
+    const answer = record.reduce((prev, curr) => {
+        const [action, uid,] = curr.split(" ");
+
+        let username = userInfo[uid];
+        if (action === ENTER) {
+            prev.push(username + ENTER_MSG);
+        } else if (action === LEAVE) {
+            prev.push(username + LEAVE_MSG);
+        } 
+
+        return prev;
+
+    }, []);
+
     console.log(answer);
 
     return answer;
 }
 
-solution(["Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"])
+solution(["Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"]);
+
+
+/*
+
+루프 한번에 출력까지 마치려고 하니 시간초과가 났다.
+유저 정보 입력, 출력 따로 생각하니 시간초과가 안 났다.
+
+*/
