a5147f6005631c4cd0762c2927a033ca331f584a
ieunjung <euju718@gmail.com>
2020-07-25 23:54:22 +0900

    짝지어 제거하기

--- programmers/0726/짝지어제거하기.js
+++ programmers/0726/짝지어제거하기.js
@@ -4,33 +4,23 @@
  * @param {string} s 문자열
  */
 function solution(s) {
-    const sArr = s.split("");
-    console.log(sArr);
 
-    while(canRemove(sArr)){
-        for (let i = 0; i < sArr.length - 1; i++) {
-            const currElement = sArr[i];
-            const nextElement = sArr[i+1];
-            if(currElement === nextElement){
-                sArr.splice(i, 2);
-            }
+    let stack = [];
+
+    for (let i = 0; i < s.length; i++) {
+        const currElement = s[i];
+
+        if (stack[stack.length - 1] === currElement) {
+            stack.pop();
+        } else {
+            stack.push(currElement);
         }
+
     }
 
-    console.log(sArr);
+    console.log(stack);
 
-    return sArr.length === 0 ? 1 : 0;
-}
-
-function canRemove(arr){
-    for (let i = 0; i < arr.length - 1; i++) {
-        const currElement = arr[i];
-        const nextElement = arr[i+1];
-        if(currElement === nextElement){
-            return true;
-        }
-    }
-    return false;
+    return stack.length === 0 ? 1 : 0;
 }
 
 solution("baabaa");
