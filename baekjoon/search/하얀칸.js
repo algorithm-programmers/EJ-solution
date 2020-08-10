8b86c22f464a9b003cc1bca2327c521a8385baa2
ieunjung <euju718@gmail.com>
2020-08-11 02:32:50 +0900

    하얀 칸

--- baekjoon/search/하얀칸.js
+++ baekjoon/search/하얀칸.js
@@ -1,42 +1,66 @@
-const readline = require('readline');
-const { rootCertificates } = require('tls');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+// 보편적인 입력 방법을 사용하면 런타임 에러가 난다.
 
-let data = [];
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
 
-rl.on('line', (line) => {
-    data.push(line.split(""));
+// let data = [];
 
-}).on('close', () => {
-    solution(data);
+// rl.on('line', (line) => {
+//     data.push(line.split(""));
 
-    process.exit();
-});
+// }).on('close', () => {
+//     solution(data);
+
+//     process.exit();
+// });
+
+var fs = require('fs');
+var lines = fs.readFileSync('/dev/stdin').toString().split("\n");
+var data = lines.map(line => line.split(""));
+solution(data);
 
 /**
  * 하얀 칸
  * . : 빈 칸
  * F : 말이 있는 칸
  * 
- * @param {string[]} data 체스판의 상태
+ * @param {string[][]} pan 체스판의 상태
  */
 function solution(pan) {
-    const countHorse = pan.reduce((prev, row, rowIdx) => {
-        let count = 0;
-        row.map((el, elIdx) => {
-            if((rowIdx % 2 === 0) && (elIdx % 2 === 0)){
-                count += 1;
-            }else if((rowIdx % 2=== 1) && (elIdx % 2 === 1)){
-                count += 1;
-            }
-        });
-        console.log(prev, row, rowIdx, count);
-        return prev + count;
-    }, 0);
+    const HORSE = 'F';
 
-    console.log(pan);
-    console.log(countHorse);
+    let count = 0;
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            const el = pan[i][j];
+            if ((i + j) % 2 === 0 && el === HORSE) {
+                count++;
+            }
+        }
+    }
+
+    console.log(count);
 }
+
+// reduce 사용
+const countHorse = pan.reduce((prev, row, rowIdx) => {
+    let count = 0;
+    for (let elIdx = 0; elIdx < row.length; elIdx++) {
+        const el = row[elIdx];
+        if (el !== HORSE) {
+            continue;
+        }
+
+        // rowIdx % 2 === 0 && elIdx % 2 === 0
+        // rowIdx % 2 === 1 && elIdx % 2 === 1
+        if ((rowIdx + elIdx) % 2 === 0) {
+            count++;
+        }
+    }
+
+    // console.log(prev, row, rowIdx, count);
+    return prev + count;
+}, 0);
