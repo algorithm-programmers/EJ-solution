b86a71ae6a89d3879aab2b77897b283dc8a0dbcb
ieunjung <euju718@gmail.com>
2020-08-19 00:12:58 +0900

    수들의 합 수정

--- baekjoon/implementation/수들의합.js
+++ baekjoon/implementation/수들의합.js
@@ -1,41 +1,52 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
 
-let data = 0;
+// let data = 0;
 
-rl.on('line', (line) => {
-    data = parseInt(line);
+// rl.on('line', (line) => {
+//     data = parseInt(line);
 
-}).on('close', () => {
-    solution(data);
-    process.exit();
-});
+// }).on('close', () => {
+//     solution(data);
+//     process.exit();
+// });
 
-// var fs = require('fs');
-// var input = fs.readFileSync('/dev/stdin').toString();
-// let data = parseInt(input);
-// solution(data);
+// 제출 코드
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString();
+let n = parseInt(input);
+let count = 1, sum = 0;
+
+while (true) {
+    sum += count;
+    if (sum > n) {
+        console.log(--count);
+        break;
+    }
+    count++;
+}
 
 /**
  * 수들의 합
  * 서로 다른 N개의 자연수의 합이 S라고 한다. S를 알 때, 자연수 N의 최댓값은 얼마일까?
+ * => 자연수 몇 개를 최대로 가져야 합을 구할 수 있는가?
  * 
  * @param {number} n 서로 다른 N개의 자연수의 합
  */
-function solution(n) {
-    let count = 1, sum = 0;
+// function solution(n) {
+//     let count = 1, sum = 0;
 
-    while (sum < n) {
-        sum += count;
-        if(sum > n){
-            count--;
-            break;
-        }
-        count++;
-    }
+//     while (sum < n) {
+//         sum += count;
+//         if (sum > n) {
+//             count--;
+//             break;
+//         }
+//         count++;
+//     }
 
-    console.log(count);
-}
+//     console.log(count);
+// }
