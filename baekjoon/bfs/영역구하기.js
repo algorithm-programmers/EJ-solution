221bd4027c2f390dadb7284eb3d942e9221d2a97
ieunjung <euju718@gmail.com>
2020-09-06 01:15:08 +0900

    영역 구하기

--- baekjoon/bfs/영역구하기.js
+++ baekjoon/bfs/영역구하기.js
@@ -7,7 +7,7 @@ const rl = readline.createInterface({
 let lines = [];
 
 rl.on('line', (line) => {
-    lines.push(line.split(" ").map(el => el/1));
+    lines.push(line.split(" ").map(el => el / 1));
 
 }).on('close', () => {
 
@@ -31,22 +31,62 @@ rl.on('line', (line) => {
  * @param {number[][]} lines 직사각형 좌표(왼쪽 아래 꼭짓점의 x, y좌표값과 오른쪽 위 꼭짓점의 x, y좌표값)
  */
 function solution(m, n, k, data) {
-    
+
     let paper = new Array(n).fill().map(el => new Array(m).fill(false));
 
-    // while(data.length > 0){
-        const curr = data.shift();
+    for (let i = 0; i < data.length; i++) {
+        const curr = data[i];
 
-        const left = {x: curr[0], y: curr[1]};
-        const right = {x: curr[2], y: curr[3]};
+        const left = { x: curr[0], y: curr[1] };
+        const right = { x: curr[2], y: curr[3] };
 
-        for (let i = left.x; i < right.x; i++) {
-            for (let j = left.y; j < right.y; j++) {
-                console.log(i, j);
+        for (let i = left.y; i < right.y; i++) {
+            for (let j = left.x; j < right.x; j++) {
                 paper[i][j] = true;
             }
         }
-    // }
+    }
 
-    console.log(paper);
+    // console.log(paper);
+
+    let area, areaList = [];
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < m; j++) {
+            if (!paper[i][j]) {
+                area = 0;
+                dfs(i, j);
+                areaList.push(area);
+            }
+        }
+    }
+
+    function dfs(x, y) {
+        // 시계 방향
+        const dx = [-1, 0, 1, 0];
+        const dy = [0, 1, 0, -1];
+
+        paper[x][y] = true;
+        area++;
+
+        for (let i = 0; i < 4; i++) {
+            const nextX = x + dx[i];
+            const nextY = y + dy[i];
+
+            if (nextX < 0 || nextY < 0 || nextX >= n || nextY >= m) {
+                continue;
+            }
+
+            if(paper[nextX][nextY]){
+                continue;
+            }
+
+            dfs(nextX, nextY);
+        }
+    }
+    
+    areaList.sort((a, b) => a - b);
+
+    // 결과 출력
+    console.log(areaList.length);
+    console.log(areaList.join(" "));
 }
