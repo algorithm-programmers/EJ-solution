5a0e6a34b6a8a65efd5c993b58ef52f2aea82e06
ieunjung <euju718@gmail.com>
2020-10-11 22:40:50 +0900

    First Missing Positive(simple)

--- leetcode/top100liked/041.js
+++ leetcode/top100liked/041.js
@@ -36,8 +36,21 @@ var firstMissingPositive = function(nums) {
     return answer;
 };
 
-// firstMissingPositive([1,2,0]);
+// simple one
+var firstMissingPositive = function(nums){
+    const max = Math.max(...nums);
+
+    for (let i = 1; i <= max + 1; i++) {
+        if(nums.indexOf(i) === -1){
+            return i;
+        }
+    }
+
+    return 1;
+}
+
+firstMissingPositive([1,2,0]);
 // firstMissingPositive([3,4,-1,1]);
 // firstMissingPositive([]);
 // firstMissingPositive([-5]);
-firstMissingPositive([0,2,2,1,1]);
+// firstMissingPositive([0,2,2,1,1]);
